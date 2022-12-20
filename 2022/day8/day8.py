78709fb4e46173d534da716ddd8f757aa6425f73
Tengokuken <Johnsonz8642@gmail.com>
2022-12-20 13:48:08 -0500

    finished day 8 wow when will i do the 12 days i missed

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -3,55 +3,113 @@ class Node:
         self.elem = elem
         self.edges = edges
         self.is_visible = is_visible
+        self.score = [0, 0, 0, 0]
 
     def __repr__(self):
-        return ("Node: " + str(self.elem) + ", edges: " + str(self.edges) + ", visible: " + str(self.is_visible))
-
+        # return ("Node: " + str(self.elem) + ", edges: " + str(self.edges) + ", visible: " + str(self.is_visible))
+        return str(self.is_visible)
 
 def neighbours_visible(graph, x, y):
-    # Check neighbours recursively
-    if rec_neighbours_visible(graph, x, y, "n") or \
-            rec_neighbours_visible(graph, x, y, "e") or \
-            rec_neighbours_visible(graph, x, y, "s") or \
-            rec_neighbours_visible(graph, x, y, "w"):
-        return True
-    return False
+    # Check if this current tree can see the end of the forest at any of its sides
+    loop_visible = True
+    # north
+    cur_x = x - 1
+    while cur_x >= 0:
+        if graph[x][y].elem <= graph[cur_x][y].elem:
+            loop_visible = False
+            break
+        cur_x -= 1
 
-def rec_neighbours_visible(graph, x, y, dir):
-    # Base case: we are at the edge of the desired direction. Return true
-    # Recursive step: Check the tree in the desired direction. Return true if you can see it from this direction
-    if dir == "n":
-        if x == 0:
-            return True
-        else:
-            return rec_neighbours_visible(graph, x-1, y, "n") and graph[x][y].elem > graph[x-1][y].elem
-    if dir == "e":
-        if y == len(graph) - 1:
-            return True
-        else:
-            return rec_neighbours_visible(graph, x, y+1, "e") and graph[x][y].elem > graph[x][y+1].elem
-    if dir == "s":
-        if x == len(graph) - 1:
-            return True
-        else:
-            return rec_neighbours_visible(graph, x+1, y, "s") and graph[x][y].elem > graph[x-1][y].elem
-    if dir == "w":
-        if y == 0:
-            return True
-        else:
-            return rec_neighbours_visible(graph, x, y-1, "w") and graph[x][y].elem > graph[x][y-1].elem
+    if loop_visible:
+        return loop_visible
+
+    loop_visible = True
+    cur_y = y + 1
+    while cur_y <= len(graph) - 1:
+        if graph[x][y].elem <= graph[x][cur_y].elem:
+            loop_visible = False
+            break
+        cur_y += 1
+
+    if loop_visible:
+        return loop_visible
+
+    loop_visible = True
+    cur_x = x + 1
+    while cur_x <= len(graph) - 1:
+        if graph[x][y].elem <= graph[cur_x][y].elem:
+            loop_visible = False
+            break
+        cur_x += 1
+
+    if loop_visible:
+        return loop_visible
+
+    loop_visible = True
+    cur_y = y - 1
+    while cur_y >= 0:
+        if graph[x][y].elem <= graph[x][cur_y].elem:
+            loop_visible = False
+            break
+        cur_y -= 1
+
+    if loop_visible:
+        return loop_visible
+
+    return loop_visible
+
+def num_neighbours_visible(graph, x, y):
+    # Check if this current tree can see the end of the forest at any of its sides
+    # north
+    cur_x = x - 1
+    count = 0
+    while cur_x >= 0:
+        count += 1
+        if graph[x][y].elem <= graph[cur_x][y].elem:
+            break
+        cur_x -= 1
+
+    graph[x][y].score[0] = count
+
+    cur_y = y + 1
+    count = 0
+    while cur_y <= len(graph) - 1:
+        count += 1
+        if graph[x][y].elem <= graph[x][cur_y].elem:
+            break
+        cur_y += 1
+
+    graph[x][y].score[1] = count
+
+    cur_x = x + 1
+    count = 0
+    while cur_x <= len(graph) - 1:
+        count += 1
+        if graph[x][y].elem <= graph[cur_x][y].elem:
+            break
+        cur_x += 1
+
+    graph[x][y].score[2] = count
+
+    cur_y = y - 1
+    count = 0
+    while cur_y >= 0:
+        count += 1
+        if graph[x][y].elem <= graph[x][cur_y].elem:
+            break
+        cur_y -= 1
+
+    graph[x][y].score[3] = count
 
 # This doesn't work but I like that I made a dp solution!!!!!
 # Because ie if north visible from west it doesnt mean that this is visible from north
 def dp_neighbours_visible(graph, x, y):
-    print("At dp step elem", graph[x][y].elem, "at", x, y)
     # Given a node, check if the neighbours are visible recursively.
     # Base case: If neighbours are visible and this tree is taller, return True
     if graph[x - 1][y].is_visible and graph[x][y].elem > graph[x - 1][y].elem or \
             graph[x][y + 1].is_visible and graph[x][y].elem > graph[x][y + 1].elem or \
             graph[x + 1][y].is_visible and graph[x][y].elem > graph[x + 1][y].elem or \
             graph[x][y - 1].is_visible and graph[x][y].elem > graph[x][y - 1].elem:
-        print("True because larger than visible neighbours")
         return True
     # Recursive step: Check north, then east, then south, then west.
     if graph[x][y].elem > graph[x-1][y].elem and dp_neighbours_visible(graph, x - 1, y):
@@ -74,36 +132,36 @@ def visible_trees(graph):
     # Traverse through all inner nodes to see what is visible
     count = 0
     for i in range(len(graph)):
-        print(graph[i])
         for j in range(len(graph[i])):
             if (graph[i][j].is_visible):
                 count += 1
     return count
 
-
-# def sig_start2(graph):
-#     # one line
-#     for i in range(len(line) - 14):
-#         substr = line[i:i+14]
-#         if len(substr) == len(set(substr)):
-#             print(substr)
-#             return i + 14
-
+def num_visible_trees(graph):
+    # Consider the inner trees only
+    highest = 0
+    for i in range(1, len(graph) - 1):
+        for j in range(1, len(graph[i]) - 1):
+            num_neighbours_visible(graph, i, j)
+            res = 1
+            for k in range(4):
+                res *= graph[i][j].score[k]
+            if res >= highest:
+                highest = res
+    return highest
 
 if __name__ == "__main__":
-    file = open('test.txt', 'r')
+    file = open('input.txt', 'r')
     lines = file.readlines()
     file.close()
 
     # Create a graph of nodes, having an edge to each neighbour
     graph = [[{} for i in range(len(lines))] for j in range(len(lines))]
-    print(lines)
     temp_lines = []
     for line in lines:
         line = line.strip('\n')
         temp_lines.append(line)
     lines = temp_lines
-    print(lines)
     for i in range(len(lines)):
         for j in range(len(lines[i])):
             node_edges = [-1, -1, -1, -1]
@@ -119,8 +177,7 @@ if __name__ == "__main__":
                 node_edges[3] = int(lines[i][j - 1])
             node = Node(lines[i][j], node_edges, visible)
             graph[i][j] = node
-    print(graph)
-    result = visible_trees(graph)
-    print(result)
-    # result = sig_start2(lines[0])
+    # result = visible_trees(graph)
+    # print(result)
+    result = num_visible_trees(graph)
     print(result)
